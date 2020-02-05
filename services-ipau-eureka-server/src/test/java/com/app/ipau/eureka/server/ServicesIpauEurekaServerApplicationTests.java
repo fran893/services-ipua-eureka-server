57f618e5ef9679d553eaca470b6c8a610099c18c
Franco Alvarez <alvarez.fr893@gmail.com>
2020-02-05 09:45:23 -0300

    Primer commit de servicio eureka server

---
+++ services-ipau-eureka-server/src/test/java/com/app/ipau/eureka/server/ServicesIpauEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.ipau.eureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicesIpauEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
