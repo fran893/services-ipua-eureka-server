8f752afdf92e69d78279a1acbc0f7f223015a4be
Franco <alvarez.fr893@gmail.com>
2020-02-06 08:20:48 -0300

    primer commit

---
+++ services-ipau-eureka-server/src/main/java/com/app/ipau/eureka/server/ServicesIpauEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.app.ipau.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesIpauEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicesIpauEurekaServerApplication.class, args);
+	}
+
+}
